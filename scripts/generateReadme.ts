2f3026cd1c7983c26e24394359c15ca2770406cb
Joseph Siddall <j.b.siddall@gmail.com>
2024-11-18 20:31:11 +0100

    updated template

---
+++ scripts/generateReadme.ts
@@ -0,0 +1,15 @@
+import Mustache from 'mustache'
+import {readFileSync, writeFileSync} from 'fs'
+
+const output = Mustache.render(
+  readFileSync('./Readme.md.mustache', 'utf-8'),
+  {
+    package: JSON.parse(readFileSync('./package.json', 'utf-8')),
+  },
+  (partialName: string) => {
+    return readFileSync(partialName, 'utf-8')
+  }
+)
+
+writeFileSync('./Readme.md', output, 'utf-8')
+console.log(output)
